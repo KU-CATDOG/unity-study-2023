f245cef45d0a32027f5048113d4921319c857215
duru100470 <zenpia@naver.com>
2023-05-30 21:45:47 +0900

    Ball 점프 구현

--- Study/Assets/Scripts/Ball.cs
+++ Study/Assets/Scripts/Ball.cs
@@ -8,6 +8,8 @@ public class Ball : MonoBehaviour
     private float acceleration;
     [SerializeField]
     private float maxSpeed;
+    [SerializeField]
+    private float jumpForce;
 
     private Rigidbody2D rigid2d;
 
@@ -18,8 +20,29 @@ public class Ball : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
         var direction = Input.GetAxisRaw("Horizontal");
 
         rigid2d.velocity = new Vector2(Mathf.Clamp(rigid2d.velocity.x + direction * acceleration, -maxSpeed, maxSpeed), rigid2d.velocity.y);
     }
+
+    private void Jump()
+    {
+        var ray = Physics2D.Raycast(transform.position, Vector2.down, 0.37f, LayerMask.GetMask("Ground"));
+
+        if (ray.collider == null) return;
+
+        rigid2d.velocity = new Vector2(rigid2d.velocity.x, 0);
+        rigid2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    // private void OnDrawGizmos()
+    // {
+    //     Debug.DrawRay(transform.position, Vector2.down * 0.36f);
+    // }
 }
